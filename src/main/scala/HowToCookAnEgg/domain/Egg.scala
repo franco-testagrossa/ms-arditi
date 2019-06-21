c315c309a572bbcfb30847753d62ee550f3c21c5
miguelemosreverte <miguellemos95@gmail.com>
2019-06-21 15:27:31 +0200

    Added domain tests
    TDD EXPLANATION AHEAD, (irrelevant to the commit message)
    
    Again, like with the api tests, I find that some functions are not yet ready for testing.
       But unlike what happened with the api tests,
       where we marked as 'pending' we figured out we could go ahead and implement them fully,
       even going to the source code and adding the neccesary implementations.
    Why? --------------------------------------------------------------------------------------
    
       _Is the function that your testing already implemented?_ (has it code inside?)
    
       *BAD*. You should write non-implemented functions first and test them.
       Why is this important?
            Because it will allow you to stay thinking about MODELING,
            and will avoid getting you into a tiresome, non-ending,
            series of labors.
    ```scala
            YOU DONT HAVE A FANTASTIC STACKTRACE in your head
            ... so don't get into endless implementation runs.
    ```
       _Is the function not-implemented AND it would be easy to implement?_
    
       *GOOD*. What does 'easy' mean?
       'easy to implement' -- as in:
            You will not have to write other functions outside of this one you are testing.
        THEN _GO GO GO_, implementation time!
            Implement it and test it in the same commit :D
    
       _Is the function not-implemented?_
    
       *GOOD*. Write a not-implemented (pending) test.
       This had the benefit of then seeing your test explode with joy once you actually implement your functions
       (oh so satisfying... :innocent:)

--- src/main/scala/HowToCookAnEgg/domain/Egg.scala
+++ src/main/scala/HowToCookAnEgg/domain/Egg.scala
@@ -1,13 +1,17 @@
-package eggs.domain
 
+package eggs.domain
 
 sealed trait Egg
 
 object Egg {
-  case object RawEgg extends Egg
+  case object RawEgg extends Egg {
+    def startCooking(style: EggStyle): PartiallyCookedEgg = PartiallyCookedEgg(style)
+  }
 
-  sealed trait CookedEgg extends Egg
+  sealed trait CookedEgg extends Egg {
+    def style: EggStyle
+  }
 
-  case class PartiallyCookedEgg (style: EggStyle) extends CookedEgg
-  case class FullyCookedEgg private[Egg](style: EggStyle) extends CookedEgg
+  case class PartiallyCookedEgg(style: EggStyle) extends CookedEgg
+  case class FullyCookedEgg(style: EggStyle) extends CookedEgg
 }
